75f8b7695872d2fdef3563ca4211b26038facf44
Pierre-Alexandre Piarulli <go@rubyrock.me>
2014-03-05 13:34:53 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RangExample::Application.config.secret_key_base = 'd0abbd601ce34f43bf98e724fc3d63a0ed4b02ecce733c4401ff1ffbb498334d49759f2ac538ec2fcb2bb8834cea54fae33a8db8f6ef0d4d1ff8a4561d56f776'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RangExample::Application.config.session_store :cookie_store, key: '_rang_example_session'
